a95861ec6fec9f389af3d3efcb22716abe1f1d8c
DOJ <omarjeng999@gmail.com>
2021-08-21 04:13:50 +0000

    Finish V1 Project

--- src/api/index.js
+++ src/api/index.js
@@ -2,17 +2,14 @@ import axios from "axios";
 
 const url = 'https://covid19.mathdro.id/api';
 
-export const fetchData = async () =>{
+export const fetchData = async (country) =>{
+    let changeableurl = url;
+    if(country){
+        changeableurl = `${url}/countries/${country}`
+    }
     try {
-        // const {data} = await axios.get(url);
-        // const modifiedData = {
-        //     confirmed: data.confirmed,
-        //     recovered: data.recovered,
-        //     deaths: data.deaths,
-        //     lastUpdate: data.lastUpdate,
-        // }
-        // Destructure it further
-        const {data:{confirmed, recovered, deaths, lastUpdate}} = await axios.get(url);
+       
+        const {data:{confirmed, recovered, deaths, lastUpdate}} = await axios.get(changeableurl);
 
         const modifiedData = {
             // confirmed: confirmed, //in JS u can ignore d other if they all av d same name
@@ -41,5 +38,16 @@ export const fetchDailyData = async () =>{
     }
     catch (error){
 
+    }
+}
+
+export const fetchCountries = async () =>{
+    try {
+        const {data: {countries}} = await axios.get(`${url}/countries`);
+
+        return countries.map((country) => country.name)
+        // console.log(response);
+    } catch (error) {
+        
     }
 }
--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -5,7 +5,7 @@ import { Line, Bar } from 'react-chartjs-2';
 
 import styles from './Chart.module.css'
 
-const Charts = () => {
+const Charts = ({ data: {confirmed, deaths, recovered}, country }) => {
     const [dailyData, setdailyData] = useState([]);
 
     useEffect(() =>{
@@ -30,7 +30,7 @@ const Charts = () => {
                                 fill: true
                             }, 
                             {
-                                data: dailyData.map(({confirmed}) => confirmed),
+                                data: dailyData.map(({deaths}) => deaths),
                                 label: 'Deaths',
                                 borderColor: 'red',
                                 backgroundColor: 'rgba(255, 0, 0, 0.5)',
@@ -42,9 +42,50 @@ const Charts = () => {
             />) : null
     );
 
+    const barChart = (
+        confirmed ? (
+            <Bar
+                data={
+                    {
+                        labels: [
+                            'Infected',
+                            'Recovered',
+                            'Deaths'
+                        ],
+                        datasets: [
+                            {
+                                label: 'People',
+                                backgroundColor: [
+                                    'rgba(0, 0, 255, 0.5)',
+                                    'rgba(0, 255, 0, 0.5)',
+                                    'rgba(255, 0, 0, 0.5)',
+                                ],
+                                data: [
+                                    confirmed.value,
+                                    recovered.value,
+                                    deaths.value
+                                ]
+                            }
+                        ]
+                    }
+                }
+                options={
+                    {
+                        legend: {
+                            display: false    
+                        },
+                        title: {
+                            display: true, text: `Current State in ${country}`
+                        }
+                    }
+                }
+            />
+        ) : null
+    )
+
     return(
         <div className={styles.container}>
-            {lineChart}
+           {country ? barChart : lineChart}
         </div>
     )
 }
